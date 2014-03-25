7a92ff8d180f5c1ac47c32966c608021d87abea7
Jake Goulding <jake.goulding@gmail.com>
2014-03-25 16:30:45 -0400

    Some basic tests

--- test/clojure_ruby/core_test.clj
+++ test/clojure_ruby/core_test.clj
@@ -1,7 +1,50 @@
 (ns clojure-ruby.core-test
   (:require [clojure.test :refer :all]
-            [clojure-ruby.core :refer :all]))
+            [clojure-ruby.core :refer :all]
+            [instaparse.core :as insta]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn unambigous? [code]
+  (= 1 (count (insta/parses ruby-parser code))))
+
+(deftest method-calls
+  (is (unambigous? "alpha.beta"))
+  (is (= (ruby-parser "alpha.beta")
+         [[:method_call_naked
+           [:var_ref "alpha"]
+           "beta"]]))
+  (is (unambigous? "alpha.beta(1, 2)"))
+  (is (= (ruby-parser "alpha.beta(1, 2)")
+         [[:method_call_args
+           [:var_ref "alpha"]
+           "beta"
+           [:number "1"]
+           [:number "2"]]]))
+  (is (unambigous? "alpha[1]"))
+  (is (= (ruby-parser "alpha[1]")
+         [[:method_call_bracket
+           [:var_ref "alpha"]
+           [:number "1"]]]))
+  (is (unambigous? "alpha < 4"))
+  (is (= (ruby-parser "alpha < 4")
+         [[:method_call_infix
+           [:var_ref "alpha"]
+           "<"
+           [:number "4"]]])))
+
+(deftest infix-and-naked
+  (is (unambigous? "alpha.beta < 4"))
+  (is (= (ruby-parser "alpha.beta < 4")
+         [[:method_call_infix
+           [:method_call_naked
+            [:var_ref "alpha"]
+            "beta"]
+           "<"
+           [:number "4"]]]))
+  (is (unambigous? "1 == gamma.delta"))
+  (is (= (ruby-parser "1 == gamma.delta")
+         [[:method_call_infix
+           [:number "1"]
+           "=="
+           [:method_call_naked
+            [:var_ref "gamma"]
+            "delta"]]])))
