663585bd215e4093abc462ab5c2ba64855ee15a9
Jake Goulding <jake.goulding@gmail.com>
2014-03-25 13:44:12 -0400

    Add support for naked calls and while
    Removed nesting from the parser around flow control

--- src/clojure_ruby/core.clj
+++ src/clojure_ruby/core.clj
@@ -6,13 +6,17 @@
 
 (def variables (atom {}))
 
+(defn create-string [s]
+  {:data s
+   :methods {"size" (fn string-size [this] (count (:data this)))}})
+
 (def Array
-  {:methods {"new" (fn [cnt val] (atom (vec (repeat cnt val))))}})
+  {:methods {"new" (fn Array-new [this cnt val] (atom (vec (repeat cnt val))))}})
 
 (swap! variables assoc "Array" Array)
 
 (def File
-  {:methods {"read" (fn [name] (slurp name))}})
+  {:methods {"read" (fn File-read [this name] (create-string (slurp name)))}})
 
 (swap! variables assoc "File" File)
 
@@ -27,7 +31,7 @@
 
 (defn rb-send [obj method-name args]
   (if-let [meth (get-in obj [:methods method-name])]
-    (apply meth args)
+    (apply meth obj args)
     (throw (RuntimeException. (str "Object " obj " does not have method " method-name)))))
 
 (defmethod evaluate :var_ref [stmt]
@@ -42,9 +46,25 @@
         args (map evaluate args)]
     (rb-send obj method args)))
 
+(defmethod evaluate :method_call_naked [stmt]
+  ;; Duplication!
+  (let [[_ obj method & args] stmt
+        obj (evaluate obj)
+        args (map evaluate args)]
+    (rb-send obj method args)))
+
 (defmethod evaluate :literal [stmt]
   (let [[_ val] stmt
         [type val] val]
     (case type
       :number (Long. val)
       :string val)))
+
+(defmethod evaluate :while [stmt]
+  (let [[_ predicate & body] stmt
+        predicate (evaluate predicate)]
+    (if predicate
+      (map evaluate body))))
+
+(defn evaluate-all [stmts]
+  (map evaluate stmts))
