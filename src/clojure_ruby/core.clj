ddd233fea02bfd93d23ce773d6795c761389afda
Jake Goulding <jake.goulding@gmail.com>
2014-03-25 13:26:16 -0400

    Evaluate up to the flow control
    This includes some basic stubs for File and Array

--- src/clojure_ruby/core.clj
+++ src/clojure_ruby/core.clj
@@ -6,6 +6,16 @@
 
 (def variables (atom {}))
 
+(def Array
+  {:methods {"new" (fn [cnt val] (atom (vec (repeat cnt val))))}})
+
+(swap! variables assoc "Array" Array)
+
+(def File
+  {:methods {"read" (fn [name] (slurp name))}})
+
+(swap! variables assoc "File" File)
+
 (defmulti evaluate first)
 
 (defmethod evaluate :method_call_infix [stmt]
@@ -15,8 +25,26 @@
     (case method
       "=" (swap! variables assoc (second obj) arg))))
 
+(defn rb-send [obj method-name args]
+  (if-let [meth (get-in obj [:methods method-name])]
+    (apply meth args)
+    (throw (RuntimeException. (str "Object " obj " does not have method " method-name)))))
+
+(defmethod evaluate :var_ref [stmt]
+  (let [[_ name] stmt]
+    (if-let [var (get @variables name)]
+      var
+      (throw (RuntimeException. (str "Cannot find variable [" name "]"))))))
+
+(defmethod evaluate :method_call_args [stmt]
+  (let [[_ obj method & args] stmt
+        obj (evaluate obj)
+        args (map evaluate args)]
+    (rb-send obj method args)))
+
 (defmethod evaluate :literal [stmt]
   (let [[_ val] stmt
         [type val] val]
     (case type
-      :number (Long. val))))
+      :number (Long. val)
+      :string val)))
