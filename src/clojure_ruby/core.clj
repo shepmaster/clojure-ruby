f7de1a3eb203519cf9513bcd1dcc2ae78770fb2c
Jake Goulding <jake.goulding@gmail.com>
2014-03-25 13:03:11 -0400

    Able to set variables to numbers

--- src/clojure_ruby/core.clj
+++ src/clojure_ruby/core.clj
@@ -3,3 +3,20 @@
 
 (def ruby-parser
   (insta/parser (clojure.java.io/resource "ruby.ebnf")))
+
+(def variables (atom {}))
+
+(defmulti evaluate first)
+
+(defmethod evaluate :method_call_infix [stmt]
+  (let [[_ obj method arg] stmt
+        arg (evaluate arg)]
+    (assert (= :var_ref (first obj)))
+    (case method
+      "=" (swap! variables assoc (second obj) arg))))
+
+(defmethod evaluate :literal [stmt]
+  (let [[_ val] stmt
+        [type val] val]
+    (case type
+      :number (Long. val))))
