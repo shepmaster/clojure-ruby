ea7b5ea7bb9f7b8a3c295e7eb519efef7e9b0281
Jake Goulding <jake.goulding@gmail.com>
2014-03-26 16:39:42 -0400

    Add Github info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-ruby "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An extremely bare-bones Ruby interpreter"
+  :url "https://github.com/shepmaster/clojure-ruby"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
