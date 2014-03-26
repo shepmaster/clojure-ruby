5b8f1b817bc4745fa6ea0a79c34e8feed38ae792
Jake Goulding <jake.goulding@gmail.com>
2014-03-25 20:42:24 -0400

    Rewrite the grammar
    This uses rule order so that regular method calls have higher
    precedence than infix operators; removes the separate case for method
    calls with no arguments, and removes the ability to call without
    parens at all.

--- test/clojure_ruby/core_test.clj
+++ test/clojure_ruby/core_test.clj
@@ -7,20 +7,20 @@
   (= 1 (count (insta/parses ruby-parser code))))
 
 (deftest method-calls
-  (is (unambigous? "alpha.beta"))
-  (is (= (ruby-parser "alpha.beta")
-         [[:method_call_naked
+  (is (unambigous? "alpha.beta()"))
+  (is (= (ruby-parser "alpha.beta()")
+         [[:method_call
            [:var_ref "alpha"]
            "beta"]]))
   (is (unambigous? "alpha.beta(1)"))
   (is (= (ruby-parser "alpha.beta(1)")
-         [[:method_call_args
+         [[:method_call
            [:var_ref "alpha"]
            "beta"
            [:number "1"]]]))
   (is (unambigous? "alpha.beta(1, 2)"))
   (is (= (ruby-parser "alpha.beta(1, 2)")
-         [[:method_call_args
+         [[:method_call
            [:var_ref "alpha"]
            "beta"
            [:number "1"]
@@ -38,19 +38,41 @@
            [:number "4"]]])))
 
 (deftest infix-and-naked
-  (is (unambigous? "alpha.beta < 4"))
-  (is (= (ruby-parser "alpha.beta < 4")
+  (is (unambigous? "alpha.beta() < 4"))
+  (is (= (ruby-parser "alpha.beta() < 4")
          [[:method_call_infix
-           [:method_call_naked
+           [:method_call
             [:var_ref "alpha"]
             "beta"]
            "<"
            [:number "4"]]]))
-  (is (unambigous? "1 == gamma.delta"))
-  (is (= (ruby-parser "1 == gamma.delta")
+  (is (unambigous? "1 == gamma.delta()"))
+  (is (= (ruby-parser "1 == gamma.delta()")
          [[:method_call_infix
            [:number "1"]
            "=="
-           [:method_call_naked
+           [:method_call
             [:var_ref "gamma"]
-            "delta"]]])))
+            "delta"]]]))
+  (is (unambigous? "a.b() && x.y()"))
+  (is (= (ruby-parser "a.b() && x.y()")
+         [[:method_call_infix
+           [:method_call
+            [:var_ref "a"]
+            "b"]
+           "&&"
+           [:method_call
+            [:var_ref "x"]
+            "y"]]]))
+  (is (unambigous? "a.b(1) && x.y(1)"))
+  (is (= (ruby-parser "a.b(1) && x.y(1)")
+         [[:method_call_infix
+           [:method_call
+            [:var_ref "a"]
+            "b"
+            [:number "1"]]
+           "&&"
+           [:method_call
+            [:var_ref "x"]
+            "y"
+            [:number "1"]]]])))
