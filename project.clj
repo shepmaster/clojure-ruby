2c3eef9d4eaf36cb1e8d82ee0cc86ed64151ac6c
Jake Goulding <jake.goulding@gmail.com>
2014-03-27 09:58:08 -0400

    Upgrade to Clojure 1.6

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/shepmaster/clojure-ruby"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [instaparse "1.3.0"]]
   :main clojure-ruby.core)
