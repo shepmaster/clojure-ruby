f9956cfefeade0ecccf9782ca49959d8496765fa
Jake Goulding <jake.goulding@gmail.com>
2014-03-28 11:57:08 -0400

    Add a local dev path for developer-specific functions

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [instaparse "1.3.0"]]
+  :profiles {:dev {:source-paths ["dev"]}}
   :main clojure-ruby.core)
